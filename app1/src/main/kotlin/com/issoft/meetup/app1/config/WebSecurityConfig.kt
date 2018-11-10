da6fcd8ef48fd07faa1fd72f21e9768372bc52a0
KirillSakolin <kirillsakolin@coherentsolutions.com>
2018-11-10 13:40:53 +0300

    webflux

--- app1/src/main/kotlin/com/issoft/meetup/app1/config/WebSecurityConfig.kt
+++ app1/src/main/kotlin/com/issoft/meetup/app1/config/WebSecurityConfig.kt
@@ -1,14 +1,49 @@
 package com.issoft.meetup.app1.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+
 
 @Configuration
-class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+@EnableWebFluxSecurity
+class WebSecurityConfig {
 
-    override fun configure(http: HttpSecurity) {
+    @Bean
+    fun userDetailsService(): MapReactiveUserDetailsService {
+        val user = User
+                .withUsername("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build()
+        return MapReactiveUserDetailsService(user)
+    }
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers(HttpMethod.GET, "/test").hasRole("USER")
+                .pathMatchers("/**").authenticated()
+                .and()
+                .httpBasic()
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    /*override fun configure(http: HttpSecurity) {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
@@ -26,6 +61,6 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
                 .withUser("admin")
                 .password("{noop}admin")
                 .roles("USER", "ADMIN")
-    }
+    }*/
 
 }
--- app1/src/main/kotlin/com/issoft/meetup/app1/controller/TestController.kt
+++ app1/src/main/kotlin/com/issoft/meetup/app1/controller/TestController.kt
@@ -4,18 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 class TestController @Autowired constructor() {
 
     @RequestMapping("/test1/{id}")
-    fun test1(@PathVariable id: String): String {
-        return "app1 " + id + " "+ System.currentTimeMillis()
+    fun test1(@PathVariable id: String): Mono<String> {
+        return Mono.just("app1 " + id + " "+ System.currentTimeMillis())
     }
 
     @RequestMapping("/test")
-    fun test(): String {
-        return "ok"
+    fun test(): Mono<String> {
+        return Mono.just("ok")
     }
 
 }
