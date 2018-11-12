fa3e50afe8120ae33e29c30c38d274d35865854f
KirillSakolin <kirillsakolin@coherentsolutions.com>
2018-11-12 17:00:33 +0300

    tmp

--- app1/src/main/kotlin/com/issoft/meetup/app1/controller/TestController.kt
+++ app1/src/main/kotlin/com/issoft/meetup/app1/controller/TestController.kt
@@ -5,11 +5,15 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import java.security.Principal
+import reactor.core.publisher.Flux
+
+
 
 @RestController
-class TestController @Autowired constructor() {
+class TestController @Autowired constructor(val webClientBuilder: WebClient.Builder) {
 
     @RequestMapping("/test1/{id}")
     fun test1(@PathVariable id: String): Mono<String> {
@@ -26,4 +30,15 @@ class TestController @Autowired constructor() {
         return Mono.just(SecurityContextHolder.getContext().authentication.name)
     }
 
+    @RequestMapping("/test4")
+    fun test4(): Mono<String> {
+        /*val accounts = webClientBuilder.build().get().uri("http://localhost:8092/read").retrieve().bodyToFlux(com.issoft.meetup.app2.User::class.java)
+        return accounts
+                .collectList()
+                .map({ a -> Customer(a) })
+                .mergeWith(repository.findById(id))
+                .collectList()
+                .map(Function { CustomerMapper.map() })*/
+        return Mono.just("qwe")
+    }
 }
--- app1/src/main/kotlin/com/issoft/meetup/app1/helper/AuthenticationHelper.kt
+++ app1/src/main/kotlin/com/issoft/meetup/app1/helper/AuthenticationHelper.kt
@@ -1,7 +1,7 @@
 package com.issoft.meetup.app1.helper
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.issoft.meetup.app1.model.User
+import com.issoft.meetup.app1.model.UserDetails
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.SimpleGrantedAuthority
@@ -15,7 +15,7 @@ class AuthenticationHelper {
                 return UsernamePasswordAuthenticationToken("default", "default", emptyList())
             }
             val jwt = JwtHelper.decode(tokenHeader.substring(7))
-            val user = ObjectMapper().readValue(jwt.claims, User::class.java)
+            val user = ObjectMapper().readValue(jwt.claims, UserDetails::class.java)
 
             return UsernamePasswordAuthenticationToken(user.user_name, "default",
                     user.authorities!!.map { role -> SimpleGrantedAuthority(role) })
